353f8e197918b554cb1631b975176196d286223c
Keesun Baik <keesun.baik@navercorp.com>
2015-01-11 14:15:52 +0900

    Override application.propereis with command-line argument

--- src/main/java/me/whiteship/boot/HomeController.java
+++ src/main/java/me/whiteship/boot/HomeController.java
@@ -1,6 +1,7 @@
 package me.whiteship.boot;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -12,13 +13,15 @@ import org.springframework.web.bind.annotation.ResponseBody;
 @Controller
 public class HomeController {
 
+    @Value("${myapp.name}")
+    private String appName;
+
     @Autowired
     private Environment environment;
 
     @RequestMapping("/")
     public @ResponseBody String home() {
-        String myAppName = environment.getProperty("myapp.name");
-        return "Welcome to " + myAppName;
+        return "Welcome to " + appName + " " + environment.getProperty("myapp.name");
     }
 
 }
