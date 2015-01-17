f3cd3c746c3943baf7d370177f2b4cfc10f0da8f
Keesun Baik <keesun.baik@navercorp.com>
2015-01-17 10:14:40 +0900

    Using YAML list

--- src/main/java/me/whiteship/boot/HomeController.java
+++ src/main/java/me/whiteship/boot/HomeController.java
@@ -1,29 +1,28 @@
 package me.whiteship.boot;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
+
+import java.util.List;
 
 /**
  * @author Keeun Baik
  */
 @Controller
-public class HomeController {
+ public class HomeController {
 
-    @Value("${myapp.name}")
-    private String name;
+    @Value("${my.servers[0]}")
+    private String server0;
 
-    @Value("${myapp.greeting}")
-    private String greeting;
-
-    @Value("${myapp.message}")
-    private String message;
-
+    @Value("${my.servers[1]}")
+    private String server1;
 
     @RequestMapping("/")
     public @ResponseBody String home() {
-        return greeting + " " + name + " " + message;
+        return server0 + " " + server1;
     }
 
 }
