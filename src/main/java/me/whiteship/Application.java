8d1d299b04a7a06488485e99941341b5b6c559d6
Keesun Baik <keesun.baik@navercorp.com>
2015-01-27 08:15:22 +0900

    Added @Controller

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -2,11 +2,13 @@ package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.ComponentScan;
 
 /**
  * @author Keeun Baik
  */
 @EnableAutoConfiguration
+@ComponentScan
 public class Application {
 
     public static void main(String[] args) {
