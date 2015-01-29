bb2fc8a7c7f81fce9801f6a2f9fdc52cb4eef0de
Keesun Baik <keesun.baik@navercorp.com>
2015-01-29 18:19:34 +0900

    Using package name as a source

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -3,16 +3,18 @@ package me.whiteship;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 /**
  * @author Keeun Baik
  */
 @EnableAutoConfiguration
 @ComponentScan
+@Configuration
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication application = new SpringApplication(Application.class);
+        SpringApplication application = new SpringApplication("me.whiteship");
         application.setBanner(new AppBanner());
         application.run(args);
     }
