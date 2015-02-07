8ca39e2593a9e63ea90a4e14a508f83d81fd343f
Keesun Baik <keesun.baik@navercorp.com>
2015-02-07 15:24:30 +0900

    Using @PreDestroy

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -4,6 +4,8 @@ import me.whiteship.eventhandlers.MyAppStartedHandler;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import javax.annotation.PreDestroy;
+
 /**
  * @author Keeun Baik
  */
@@ -14,5 +16,10 @@ public class Application {
         SpringApplication application = new SpringApplication(Application.class);
         application.addListeners(new MyAppStartedHandler());
         application.run(args);
+    }
+
+    @PreDestroy
+    public void destroy() {
+        System.out.println("Good Luck!");
     }
 }
