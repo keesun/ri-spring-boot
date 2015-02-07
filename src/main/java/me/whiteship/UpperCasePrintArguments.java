a5887e23dd42ad97329a35b3b0cda3df2b927a1f
Keesun Baik <keesun.baik@navercorp.com>
2015-02-07 14:20:13 +0900

    Using @Order with CommandLineRunner

--- src/main/java/me/whiteship/UpperCasePrintArguments.java
+++ src/main/java/me/whiteship/UpperCasePrintArguments.java
@@ -1,18 +1,20 @@
 package me.whiteship;
 
 import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 /**
  * @author Keeun Baik
  */
 @Component
-public class MyBean implements CommandLineRunner {
+@Order(2)
+public class UpperCasePrintArguments implements CommandLineRunner {
 
     @Override
     public void run(String... args) throws Exception {
         for (String arg : args) {
-            System.out.println(arg);
+            System.out.println(arg.toUpperCase());
         }
     }
 }
