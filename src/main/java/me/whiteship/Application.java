3ad112287172cba5d21e768f1f491dd03211c0c6
Keesun Baik <keesun.baik@navercorp.com>
2015-02-13 08:11:21 +0900

    Set profiles by using setEnvironment

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -2,6 +2,8 @@ package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.context.support.StandardServletEnvironment;
 
 /**
  * @author Keeun Baik
@@ -10,10 +12,13 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
-        SpringApplication app = new SpringApplication();
-        app.setAdditionalProfiles();
+        SpringApplication app = new SpringApplication(Application.class);
 
-        SpringApplication.run(Application.class, args);
+        ConfigurableEnvironment environment = new StandardServletEnvironment();
+        environment.setActiveProfiles("dev");
+        app.setEnvironment(environment);
+
+        app.run(args);
     }
 
 }
