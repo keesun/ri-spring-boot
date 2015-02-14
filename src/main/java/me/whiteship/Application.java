67bd6bb002d4353b1292cd1802aefa6d06022c75
Keesun Baik <keesun.baik@navercorp.com>
2015-02-14 17:18:59 +0900

    Logging: console output

--- src/main/java/me/whiteship/Application.java
+++ src/main/java/me/whiteship/Application.java
@@ -1,6 +1,7 @@
 package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.ansi.AnsiOutput;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
@@ -10,6 +11,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class Application {
 
     public static void main(String[] args) {
+        AnsiOutput.setEnabled(AnsiOutput.Enabled.ALWAYS);
         SpringApplication.run(Application.class, args);
     }
 
